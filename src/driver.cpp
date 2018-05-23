a07b21a425fce79b71964b80e58aa4f0448b8461
felpsisonfire <felipeanndrade@gmail.com>
2018-05-22 22:56:24 -0300

    DONE(?)

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,7 +6,7 @@
 
 template <typename T>
 void print_list(ls::list<T> A ){
-	std::cout << ">>> Original array: [ ";
+	std::cout << ">>> Current List: [ ";
 	ls::copy( A.begin(), A.end(), std::ostream_iterator< int >( std::cout, " " ) );
 	std::cout << "]\n";
 }
@@ -26,18 +26,14 @@ int main(int argc, char *argv[])
 	print_list(lista);
 
 	std::cout << "Teste insert initializer list" << std::endl;
-	for (auto i = lista.begin(); i != lista.end(); ++i){
-		std::cout << *i << std::endl;
-	}
+	print_list(lista);	
 	std::cout <<  std::endl;
 	std::cout << "Teste retorno insert: initializer list: " << *x << std::endl;
 
 	x = lista.insert(lista.begin(),lista.begin()+2,lista.end());
 	
 	std::cout << "Teste insert [first,last) " << std::endl;
-	for (auto i = lista.begin(); i != lista.end(); ++i){
-		std::cout << *i << std::endl;
-	}
+	print_list(lista);
 	std::cout << "Teste retorno insert [first,last): " << *x << std::endl;
 	
 	std::cout << "Teste size: " << lista.size() << std::endl; 
@@ -46,15 +42,11 @@ int main(int argc, char *argv[])
 	std::cout << "Teste erase pos "<< *x << std::endl;
 	std::cout <<  std::endl;
 
-	for (auto i = lista.begin(); i != lista.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista);
 
 	x = lista.erase(lista.begin()+3,lista.end());
 	std::cout << "Teste erase  [first,last) " << std::endl;
-	for (auto i = lista.begin(); i != lista.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista);
 	//lista.clear();
 	//std::cout << "list size after clear : " << lista.size() << std::endl;
 	
@@ -62,41 +54,32 @@ int main(int argc, char *argv[])
 	ls::list<int> lista2{{1,2,3,4}};
 	std::cout << "list2 itializer list : " << lista2.size() << std::endl;
 
-	for (auto i = lista2.begin(); i != lista2.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista2);
 	lista2.assign(5,7);
 	std::cout << "list2 assign: "  << std::endl;
-	for (auto i = lista2.begin(); i != lista2.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	
+	print_list(lista2);	
 
 	lista2.assign(lista.begin(),lista.end());
 	
 	std::cout << "list2 assign [first,last): "  << std::endl;
-	for (auto i = lista2.begin(); i != lista2.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	
+	print_list(lista2);
 
 	lista2.assign({1,2,3,4,5});
 	
 	std::cout << "list2 assign initializer list: "  << std::endl;
-	for (auto i = lista2.begin(); i != lista2.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista2);	
 
 	ls::list<int> lista3 = lista2;
 	std::cout << "list3 assignment operator: "  << std::endl;
-	for (auto i = lista3.begin(); i != lista3.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista3);
 	
 	ls::list<int> lista4(lista3);
 	
 	std::cout << "list4 copy constructor: "  << std::endl;
-	for (auto i = lista4.begin(); i != lista4.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista4);
+	
 	std::cout << "list3 size : " << lista3.size() << std::endl;
 	std::cout << "list4 size : " << lista4.size() << std::endl;
 
@@ -105,20 +88,13 @@ int main(int argc, char *argv[])
 	std::cout << "Teste operator lista4 != lista3 " << (lista4 != lista3) << std::endl;
 
 	ls::list<int> lista5(5);
-	for (auto i = lista5.begin(); i != lista5.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista5);	
 	
 	std::cout << "Before pop front and back " << std::endl;
-	for (auto i = lista3.begin(); i != lista3.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
-	
+	print_list(lista3);	
 	std::cout << "Teste pop front and back " << std::endl;
 	lista3.pop_front();
 	lista3.pop_back();
-	for (auto i = lista3.begin(); i != lista3.end(); ++i) {
-		std::cout << *i << std::endl;
-	}
+	print_list(lista3);
 	return 0;
 }
